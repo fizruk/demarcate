4ff6f26eb3f1bd7c0191317b8837b9c64b184889
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-09-20 13:47:52 +0000

    comments for simple example

--- examples/simple.hs
+++ examples/simple.hs
@@ -8,45 +8,51 @@ import Control.Monad.Trans.Demarcate
 import Control.Monad.Free
 import Control.Monad.State
 
+-- | Instructions for a toy language.
 data ProgramF next
-  = Output String next
-  | Input (String -> next)
+  = Output String next      -- output something
+  | Input (String -> next)  -- get something
 
 instance Functor ProgramF where
     fmap f (Output s x) = Output s (f x)
     fmap f (Input g)    = Input (f . g)
 
+-- | Low-level program is just a Free monad over ProgramF.
 type Program = Free ProgramF
 
+-- | Output command.
 output :: (MonadFree ProgramF m, MonadTrans t) => String -> t m ()
 output s = lift . liftF $ Output s ()
 
+-- | Input command.
 input :: (MonadFree ProgramF m, MonadTrans t) => t m String
 input = lift . liftF $ Input id
 
+-- | SP stands for Stateful Program.
 type SP = Demarcate (StateT Int) Program
 
 instance (MonadState s (t m)) => MonadState s (Demarcate t m) where
     get = demarcateT get
     put = demarcateT . put
 
+-- | Interpreter for a low-level program.
 runProgram :: Program a -> IO a
 runProgram = iterM runProgramF
   where
     runProgramF (Output s next) = putStrLn s >> next
     runProgramF (Input next)    = getLine >>= next
 
+-- | Interpreter for a stateful program
 runSP :: SP a -> IO a
-runSP = runProgram . flip evalStateT 10 . execDemarcate
+runSP = runProgram . flip evalStateT 0 . execDemarcate
 
-wrapCmd :: (Functor f, MonadTrans t, Monad (t (Free f))) => f (t (Free f) a) -> t (Free f) a
-wrapCmd = join . lift . liftF
-
+-- | Subprogram. Outputs current state.
 outputState :: SP ()
 outputState = do
     n <- get
     output $ "state: " ++ show n
 
+-- | Program for our toy language.
 prg :: SP ()
 prg = do
     outputState
@@ -58,17 +64,21 @@ prg = do
 
     outputState
 
+-- | A hacking transformation.
+-- It prepend to each low-level output some extra stateful code.
+-- Other commands are left untouched.
 hack :: SP a -> SP a
-hack = transformDemarcateM $ iterM hackF
+hack = transformDemarcateFree hackF
   where
     hackF :: ProgramF (SP a) -> SP a
     hackF cmd@(Output s next) = do
       modify (+1)
       output "*** incremented state"
-      wrapCmd cmd
+      wrapInner cmd
     hackF cmd =
-      wrapCmd cmd
+      wrapInner cmd
 
+-- | Running hacked program...
 main :: IO ()
 main = runSP $ hack prg
 
