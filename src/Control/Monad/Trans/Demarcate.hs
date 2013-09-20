e479b7d947b5dac3b2dde108377c1b3f26de3b94
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-09-20 13:19:11 +0000

    reexport + transformDemacrateM

--- src/Control/Monad/Trans/Demarcate.hs
+++ src/Control/Monad/Trans/Demarcate.hs
@@ -1,3 +1,5 @@
-module Control.Monad.Trans.Demarcate where
+module Control.Monad.Trans.Demarcate (
+  module Control.Monad.Trans.Demarcate.Internal,
+) where
 
 import Control.Monad.Trans.Demarcate.Internal
--- src/Control/Monad/Trans/Demarcate/Internal.hs
+++ src/Control/Monad/Trans/Demarcate/Internal.hs
@@ -4,8 +4,10 @@
 
 module Control.Monad.Trans.Demarcate.Internal (
     Demarcate,
+    demarcateM, demarcateT,
     execDemarcate,
-    hoistDemarcate,
+    hoistDemarcateT,
+    transformDemarcateM,
 ) where
 
 import Control.Monad.Free
@@ -42,14 +44,22 @@ demarcateT :: t m a -> Demarcate t m a
 demarcateT m = liftF $ DemarcateTrans m id
 
 execDemarcate :: (Monad (t m), Monad m, MonadTrans t) => Demarcate t m a -> t m a
-execDemarcate = iter execDemarcateF . fmap return . unDemarcate
+execDemarcate = iterM execDemarcateF . unDemarcate
   where
     execDemarcateF (DemarcateMonad m next) = lift m >>= next
     execDemarcateF (DemarcateTrans m next) = m >>= next
 
-hoistDemarcate :: (forall b. t m b -> t' m b) -> Demarcate t m a -> Demarcate t' m a
-hoistDemarcate phi = iter hoistDemarcateF . fmap return . unDemarcate
+hoistDemarcateT :: (forall b. t m b -> t' m b) -> Demarcate t m a -> Demarcate t' m a
+hoistDemarcateT phi = iterM hoistDemarcateF . unDemarcate
   where
     hoistDemarcateF (DemarcateMonad m next) = demarcateM m >>= next
     hoistDemarcateF (DemarcateTrans m next) = demarcateT (phi m) >>= next
 
+transformDemarcateM :: (forall b. m b -> Demarcate t m b) -> Demarcate t m a -> Demarcate t m a
+transformDemarcateM phi = iterM transformDemarcateF . unDemarcate
+  where
+    transformDemarcateF (DemarcateMonad m next) = phi m >>= next
+    transformDemarcateF (DemarcateTrans m next) = demarcateT m >>= next
+
+wrapInner :: Functor f => f (t (Free f) a) -> t (Free f) a
+wrapInner = join . lift . liftF
