3277776597a870974be775f65280f1ca103b7bd5
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-09-20 19:54:42 +0000

    church encoding for Demarcate

--- src/Control/Monad/Trans/Demarcate/Internal.hs
+++ src/Control/Monad/Trans/Demarcate/Internal.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
@@ -6,77 +6,59 @@ module Control.Monad.Trans.Demarcate.Internal (
     Demarcate,
     demarcateM, demarcateT,
     execDemarcate,
-    wrapInner,
-    transformDemarcateM, transformDemarcateFree,
-    hoistDemarcateT,
+    transformDemarcateM,
 ) where
 
-import Control.Monad.Free
 import Control.Monad.Trans.Class
-import Control.Monad (join)
 
--- | Demarcate functor.
-data DemarcateF t m next
-    = forall a. DemarcateMonad (  m a) (a -> next)  -- | Unlifted monadic computation.
-    | forall a. DemarcateTrans (t m a) (a -> next)  -- | Transformed monadic computation.
-
-instance Functor (DemarcateF t m) where
-    fmap f (DemarcateMonad m g) = DemarcateMonad m (f . g)
-    fmap f (DemarcateTrans m g) = DemarcateTrans m (f . g)
-
--- | Demarcate monad transformer.
 newtype Demarcate t m a = Demarcate
-    { unDemarcate :: Free (DemarcateF t m) a }
+    { unDemarcate :: forall r.
+        (a -> r) ->
+        (forall b.   m b -> (b -> Demarcate t m a) -> r) ->
+        (forall b. t m b -> (b -> Demarcate t m a) -> r) ->
+        r }
 
 instance Functor (Demarcate t m) where
-    fmap f = Demarcate . fmap f . unDemarcate
+    fmap f (Demarcate g) = Demarcate $ \rf mf tf ->
+      g (rf . f)
+        (\mm mh -> mf mm (fmap f . mh))
+        (\tm th -> tf tm (fmap f . th))
 
 instance Monad (Demarcate t m) where
-    return  = Demarcate . return
-    m >>= f = Demarcate $ unDemarcate m >>= unDemarcate . f
-
-instance MonadFree (DemarcateF t m) (Demarcate t m) where
-    wrap = Demarcate . wrap . fmap unDemarcate
+    return x = Demarcate $ \rf _ _ -> rf x
+    Demarcate g >>= f =
+      g f
+        (\m h -> demarcateMonad m ((>>= f) . h))
+        (\m h -> demarcateTrans m ((>>= f) . h))
 
 instance MonadTrans (Demarcate t) where
-    lift m = liftF $ DemarcateMonad m id
+    lift = demarcateM
+
+demarcateMonad :: m b -> (b -> Demarcate t m a) -> Demarcate t m a
+demarcateMonad m g = Demarcate $ \_ mf _ -> mf m g
+
+demarcateTrans :: t m b -> (b -> Demarcate t m a) -> Demarcate t m a
+demarcateTrans m g = Demarcate $ \_ _ tf -> tf m g
 
 -- | Lift pure monadic computation into @Demarcate t m a@
 demarcateM :: m a -> Demarcate t m a
-demarcateM m = liftF $ DemarcateMonad m id
+demarcateM m = demarcateMonad m return
 
 -- | Lift transformed monadic computation into @Demarcate t m a@
 demarcateT :: t m a -> Demarcate t m a
-demarcateT m = liftF $ DemarcateTrans m id
+demarcateT m = demarcateTrans m return
 
 -- | Execute demarcated computation.
 execDemarcate :: (Monad (t m), Monad m, MonadTrans t) => Demarcate t m a -> t m a
-execDemarcate = iterM execDemarcateF . unDemarcate
-  where
-    execDemarcateF (DemarcateMonad m next) = lift m >>= next
-    execDemarcateF (DemarcateTrans m next) = m >>= next
-
--- | Subsitute monad transformer.
-hoistDemarcateT :: (forall b. t m b -> t' m b) -> Demarcate t m a -> Demarcate t' m a
-hoistDemarcateT phi = iterM hoistDemarcateF . unDemarcate
-  where
-    hoistDemarcateF (DemarcateMonad m next) = demarcateM m >>= next
-    hoistDemarcateF (DemarcateTrans m next) = demarcateT (phi m) >>= next
+execDemarcate (Demarcate g) =
+    g return
+      (\m h -> lift m >>= execDemarcate . h)
+      (\m h -> m >>= execDemarcate . h)
 
 -- | Substitute monad computations with demarcated.
 transformDemarcateM :: (forall b. m b -> Demarcate t m b) -> Demarcate t m a -> Demarcate t m a
-transformDemarcateM phi = iterM transformF . unDemarcate
-  where
-    transformF (DemarcateMonad m next) = phi m >>= next
-    transformF (DemarcateTrans m next) = demarcateT m >>= next
-
--- | Substitute free monad actions with demarcated monad computations.
-transformDemarcateFree :: (Functor f) =>
-  (forall b. f (Demarcate t (Free f) b) -> Demarcate t (Free f) b) -> Demarcate t (Free f) a -> Demarcate t (Free f) a
-transformDemarcateFree phi = transformDemarcateM (iterM phi)
-
--- | Helper function (useful with @transformDemarcateFree@).
--- I believe it should be somewhere in @Control.Monad.Free@
-wrapInner :: (Functor f, MonadTrans t, Monad (t (Free f))) => f (t (Free f) a) -> t (Free f) a
-wrapInner = join . lift . liftF
+transformDemarcateM phi (Demarcate g) =
+    g return
+      (\m h -> phi m >>= transformDemarcateM phi . h)
+      (\m h -> demarcateTrans m (transformDemarcateM phi . h))
 
